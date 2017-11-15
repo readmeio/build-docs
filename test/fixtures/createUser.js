7db1644601dfb14766f28aea33ce688553e5156f
Marc Cuva <mjcuva@gmail.com>
2017-11-15 15:44:08 -0800

    Add support for file type

--- test/fixtures/createUser.js
+++ test/fixtures/createUser.js
@@ -8,6 +8,7 @@
  * @param {string} name="Marc Cuva" Name of the user
  * @param {number} age=12 Age of the user
  * @param {string[]} interests Interests of the user
+ * @param {file} profileImage Profile picture of the user
  * @throws {ValidationError} Must provide all required fields
  * @error {ValidationError2} Must provide all required fields
  * @returns {Object} The saved user
